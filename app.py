35ef8f3c0a39ec572a06fb169d6734aad67e3548
Upsham Naik <upshamgnaik@outlook.com>
2019-07-24 07:02:40 -0400

    added both my heroku apps

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import time, threading, http.client, json
 
-def index():
-    WAIT_SECONDS = 300
+WAIT_SECONDS = 300
+connStrings = ['mm19bot.herokuapp.com', 'mm19survey.herokuapp.com', '', '']
 
-    conn = http.client.HTTPSConnection('mm19bot.herokuapp.com')
-
+def makeConn(connString):
+    conn = http.client.HTTPSConnection(connString)
     headers = {'Content-type': 'application/json'}
-
     params = {"caffeinate" : "la colombe"}
     json_data = json.dumps(params)
-
     conn.request('POST', '/caffeinate', json_data, headers)
-
     response = conn.getresponse()
     print(response.read().decode())
-    #threading.Timer(WAIT_SECONDS, ping).start()
     conn.close()
     threading.Timer(WAIT_SECONDS, index).start()
+
+def index():
+    makeConn(connStrings[0])
+    makeConn(connStrings[1])
 
 if __name__ == '__main__':
     index()
